6d1680031fd8aabd8efda664589ffed014984f6e
maicol <maicol.matteoli@gmail.com>
2021-09-01 13:42:34 +0200

    add : 2 objects and properties output

--- index.php
+++ index.php
@@ -3,17 +3,31 @@
 class Movie{
     public $genre; //genere film
     public $date; //data di uscita
+    public $time;
 
+    // constructor
     public function __construct($_genre , $_date){
         $this->genre = $_genre;
         $this->date = $_date;
     }
 
+    // getter & setter
+    public function setTime($time){
+        $this->time= $time;
+    }
+    public function getTime(){
+        return $this->time;
+    }
 }
 
 $film = new Movie("asd" , "20/02/2001");
+$film2 = new Movie("ciao" , "22/02/2012");
 
 var_dump($film);
+var_dump($film2);
 
+$film2->setTime(120);
 
+var_dump($film2);
+echo $film2->genre , " ",$film2->date ," " ,$film2->time ; //test output con echo 
 ?>
